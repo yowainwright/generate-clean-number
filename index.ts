c18f8b4066cb56ee665fde1c9dab99fcc4b5548c
Jeff Wainwright <yowainwright@gmail.com>
2019-02-11 11:23:30 -0800

    [init] adds `filterNaNfromNumber`

--- index.ts
+++ index.ts
@@ -2,9 +2,24 @@
   * generateCleanNumber
   * {num} Number
   * reparse num if NaN (happens in IE)
+  * regex pattern
   */
-const generateCleanNumber = (num: any): number => {
+export const generateCleanNumber = (num: any): number => {
   return isNaN(num)
   ? parseInt(num.toString().replace(/[^0-9]/g, ''), 10)
   : num
+}
+
+/**
+  * const filterNaNfromNumber
+  * {num} Number
+  * reparse num if NaN (happens in IE)
+  * array filter pattern
+  */
+export const filterNaNfromNumber = (num: any): number => {
+  if (typeof num === 'number') return num 
+  const numArray = number.split('')
+  const filteredNumArray = num.filter(char => typeof char === 'number')
+  const filteredNumString = num.join('')
+  return parseInt(filteredNumString, 10)
 }
