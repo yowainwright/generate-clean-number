88234b0fe27477d9afbed2cbc2f421aa2399803d
Jeff Wainwright <yowainwright@gmail.com>
2019-02-20 22:29:35 -0800

    [init] adds initial test work

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -2,3 +2,33 @@ import {
   generateCleanNumber,
   filterNaNfromNumber.
 } from '..'
+
+const scenarios = [
+  {
+    input: '12^~',
+    output: 12, 
+  },
+  {
+    input: 13,
+    output: 13,
+  },
+  {
+    input: '1^2',
+    output: 12,
+  }, {
+    input: 14,
+    output: 14,
+  }
+]
+
+scenarios.forEach(function testScenario (scenario) {
+  test(`test generateCleanNumber`, () => {
+    const result = generateCleanNumber(scenario.input)
+    expect(result).toBe(scenario.output)
+  })
+
+  test(`test filterNaNfromNumber`, () => {
+    const result = filterNaNfromNumber(scenario.input)
+    expect(result).toBe(scenario.output)
+  })
+})
--- index.ts
+++ index.ts
@@ -1,21 +1,9 @@
-/**
-  * generateCleanNumber
-  * {num} Number
-  * reparse num if NaN (happens in IE)
-  * regex pattern
-  */
-export const generateCleanNumber = (num: number): number => {
+export const generateCleanNumber = (num: any): number => {
   return isNaN(num)
   ? parseInt(num.toString().replace(/[^0-9]/g, ''), 10)
   : num
 }
 
-/**
-  * const filterNaNfromNumber
-  * {num} Number
-  * reparse num if NaN (happens in IE)
-  * array filter pattern
-  */
 export const filterNaNfromNumber = (num: any): number => {
   if (typeof num === 'number') return num 
   const numArray: [] = num.split('')
