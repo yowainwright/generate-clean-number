7e5b671ada045142fb6627c3c7a7432c129ca795
yowainwright <yowainwright@gmail.com>
2022-06-05 23:40:50 -0700

    fix: remove deps, etc

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,2 @@
 export declare const generateCleanNumber: (num: string | number) => number;
 export declare const filterNaNfromNumber: (num: string | number) => number;
-//# sourceMappingURL=index.d.ts.map
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateCleanNumber = function (num) {
+exports.filterNaNfromNumber = exports.generateCleanNumber = void 0;
+var generateCleanNumber = function (num) {
     return typeof num !== "string" || isNaN(Number(num))
         ? parseFloat(num.toString().replace(/[^0-9,.]/g, ""))
         : parseFloat(num);
 };
-exports.filterNaNfromNumber = function (num) {
+exports.generateCleanNumber = generateCleanNumber;
+var filterNaNfromNumber = function (num) {
     if (typeof num === "number")
         return num;
     var numArray = num.split("");
@@ -13,4 +15,4 @@ exports.filterNaNfromNumber = function (num) {
     var filteredNumString = filteredNumArray.join("");
     return parseFloat(filteredNumString);
 };
-//# sourceMappingURL=index.js.map
+exports.filterNaNfromNumber = filterNaNfromNumber;
